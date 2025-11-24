1a243a3a9de5efd88e46db022d95428d1ca08ae0
BUDDHA <83627236+buddha784@users.noreply.github.com>
2025-11-24 23:14:22 +0530

    Create w7q2.py

---
+++ w7q2.py
@@ -0,0 +1,18 @@
+#  Write a function for Perfect Powers Check is_perfect_power(n) that checks if a number can be expressed as ab where a > 0 and b > 1.
+import time
+import tracemalloc
+
+def polygonal_number(s, n):
+    return ((s - 2) * n * n - (s - 4) * n) // 2
+s = int(input("Enter s (number of sides): "))
+n = int(input("Enter n (term number): "))
+tracemalloc.start()
+start = time.time()
+result = polygonal_number(s, n)
+current, peak = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+exec_time = time.time() - start
+
+print(f"\n{n}-th {s}-gonal number = {result}")
+print(f"Execution Time: {exec_time:.6f} seconds")
+print(f"Memory Used: {peak/1024:.2f} KB")
